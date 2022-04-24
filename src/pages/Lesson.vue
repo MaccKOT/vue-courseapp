8195fa73b1bef45340c1e2fca4ed86769bb745e3
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2022-04-24 14:52:00 +0400

    Add course and lesson component

---
+++ src/pages/Lesson.vue
@@ -0,0 +1,10 @@
+<script setup>
+import courses from '../courses';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const { courseId, lessonId } = route.params;
+const course = courses.find((course) => course.id === parseInt(courseId));
+const lesson = course.lessons.find(
+  (lesson) => lesson.id === parseInt(lessonId)
+);
+</script>
